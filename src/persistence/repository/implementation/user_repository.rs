1fea17ca19ca988eb84fed5c5c85eb2e54d1819f
leoneltrich <leoneltrich@icloud.com>
2025-12-07 01:15:06 +0100

    finished user repository

--- src/persistence/repository/implementation/user_repository.rs
+++ src/persistence/repository/implementation/user_repository.rs
@@ -9,7 +9,7 @@ pub struct SqliteUserRepository<'a> {
 }
 
 impl<'a> UserRepository for SqliteUserRepository<'a> {
-    fn get_user_by_name(&self, username: &str) -> Result<Option<User>> {
+    fn get_user(&self, username: &str) -> Result<Option<User>> {
         let mut stmt = self.conn.prepare(
             "SELECT username, password_hash FROM users WHERE username = ?1"
         )?;
@@ -21,5 +21,27 @@ impl<'a> UserRepository for SqliteUserRepository<'a> {
             })
         })
             .optional()
+    }
+
+    fn update_user(&self, user: &User) -> Result<usize> {
+        self.conn.execute(
+            "UPDATE users
+             SET password_hash = ?1
+             WHERE username = ?2",
+            [&user.password_hash, &user.username],
+        )
+    }
+    fn delete_user(&self, username: &str) -> Result<usize> {
+        self.conn.execute(
+            "DELETE FROM users WHERE username = ?1",
+            [username],
+        )
+    }
+
+    fn create_user(&self, user: &User) -> Result<usize> {
+        self.conn.execute(
+            "INSERT INTO users (username, password_hash) VALUES (?1, ?2)",
+            [&user.username, &user.password_hash],
+        )
     }
 }
--- src/persistence/repository/interface/user_repository.rs
+++ src/persistence/repository/interface/user_repository.rs
@@ -4,5 +4,8 @@ use rusqlite::Result;
 use crate::model::user::User; // <-- Import from top-level model
 
 pub trait UserRepository {
-    fn get_user_by_name(&self, username: &str) -> Result<Option<User>>;
+    fn get_user(&self, username: &str) -> Result<Option<User>>;
+    fn update_user(&self, user: &User) -> Result<usize>;
+    fn delete_user(&self, username: &str) -> Result<usize>;
+    fn create_user(&self, user: &User) -> Result<usize>;
 }
