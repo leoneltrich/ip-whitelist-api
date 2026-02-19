becf1a481713beaabbf85498fd3f37698d441309
leoneltrich <leoneltrich@icloud.com>
2026-02-19 20:20:42 +0100

    Added logging in auth service service utils

--- crates/auth_service/src/api/services/auth/utils.rs
+++ crates/auth_service/src/api/services/auth/utils.rs
@@ -8,6 +8,7 @@ use shared::auth::jwt;
 use shared::auth::models::Claims;
 use shared::errors::app_errors::AppError;
 use sqlx::Error;
+use tracing::{debug, error};
 
 const THIRTY_DAYS_IN_SECONDS: i64 = 60 * 60 * 24 * 30;
 
@@ -19,6 +20,8 @@ pub fn create_access_token(private_key_pem: &String, user: &User) -> Result<Stri
     let claims = Claims::new(user.username.clone(), user.is_admin);
 
     let access_token = jwt::sign(claims, private_key_pem)?;
+
+    debug!("Successfully generated access token");
     Ok(access_token)
 }
 
@@ -27,13 +30,17 @@ pub async fn create_refresh_token(
     refresh_token_repository: &dyn RefreshTokenRepository,
 ) -> Result<String, AppError> {
     let refresh_token = generate_plain_token();
+
     save_refresh_token(&refresh_token, username, refresh_token_repository)
         .await
-        .map_err(|_| {
+        .map_err(|e| {
+            error!("An error occurred saving the refresh token: {}", e);
             AppError::InternalServerError(
                 "An error occurred during the refresh token generation".to_string(),
             )
         })?;
+
+    debug!("Successfully generated refresh token");
     Ok(refresh_token)
 }
 
@@ -54,6 +61,7 @@ async fn save_refresh_token(
         is_revoked: false,
     };
 
+    debug!("Finished preparing refresh token, saving...");
     refresh_token_repository
         .create_refresh_token(&refresh_token)
         .await
@@ -69,7 +77,8 @@ pub async fn get_user_optional(
     repository: &dyn UserRepository,
     username: &String,
 ) -> Result<Option<User>, AppError> {
-    let user_option = repository.get_user_by_name(username).await.map_err(|_| {
+    let user_option = repository.get_user_by_name(username).await.map_err(|e| {
+        error!("An error occurred accessing the database: {}", e);
         AppError::InternalServerError("An internal server error occurred".to_string())
     })?;
     Ok(user_option)
